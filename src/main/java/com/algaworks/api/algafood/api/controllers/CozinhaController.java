2f7c64a7bcd078bc69b49eb26da89c89507735ec
Jota Santos <jailsonsantosdpge@outlook.com>
2025-02-11 16:13:14 -0400

    4.23. Modelando e implementando a inclusão de recursos com POST

--- src/main/java/com/algaworks/api/algafood/api/controllers/CozinhaController.java
+++ src/main/java/com/algaworks/api/algafood/api/controllers/CozinhaController.java
@@ -3,14 +3,12 @@ package com.algaworks.api.algafood.api.controllers;
 import com.algaworks.api.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.api.algafood.domain.model.Cozinha;
 import com.algaworks.api.algafood.domain.services.CozinhaService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,12 +31,25 @@ public class CozinhaController {
     @GetMapping("/{id}")
     public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
         Cozinha cozinha = cozinhaService.buscar(id);
-        return ResponseEntity.status(HttpStatus.OK).body(cozinha); // status depois o body ou build se não quiser corpo
+
+        if (cozinha == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // status depois o body ou build se não quiser corpo
+        return ResponseEntity.status(HttpStatus.OK).body(cozinha);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
     public CozinhasXmlWrapper listarXml() {
         List<Cozinha> cozinhas = cozinhaService.listar();
         return new CozinhasXmlWrapper(cozinhas);
+    }
+
+    @PostMapping
+    public ResponseEntity<Cozinha> salvar(@RequestBody @Valid Cozinha cozinha) {
+        Cozinha cozinhaCreated = cozinhaService.salvar(cozinha);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cozinhaCreated);
     }
 }
--- src/main/java/com/algaworks/api/algafood/domain/services/CozinhaService.java
+++ src/main/java/com/algaworks/api/algafood/domain/services/CozinhaService.java
@@ -2,10 +2,12 @@ package com.algaworks.api.algafood.domain.services;
 
 import com.algaworks.api.algafood.domain.model.Cozinha;
 import com.algaworks.api.algafood.domain.repository.CozinhaRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -17,14 +19,16 @@ public class CozinhaService {
     public Cozinha buscar(Long id) {
         Optional<Cozinha> cozinhaOptional = cozinhaRepository.findById(id);
 
-        if (cozinhaOptional.isPresent()) {
-            return cozinhaOptional.get();
-        }
-
-        throw new RuntimeException("Registro não encontrado");
+        return cozinhaOptional.orElse(null);
     }
 
     public List<Cozinha> listar() {
         return cozinhaRepository.findAll();
+    }
+
+    @Transactional
+    public Cozinha salvar(Cozinha cozinha) {
+        Objects.requireNonNull(cozinha, "Cozinha não pode ser null");
+        return cozinhaRepository.save(cozinha);
     }
 }
