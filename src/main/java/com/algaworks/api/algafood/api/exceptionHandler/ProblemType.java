e64c0e54cabfa9e733dd79e922c00450147fddfe
Jota Santos <jailsonsantosdpge@outlook.com>
2025-07-04 17:19:33 -0400

    8.25. Desafio: tratando exception de parâmetro de URL inválido

--- src/main/java/com/algaworks/api/algafood/api/exceptionHandler/ProblemType.java
+++ src/main/java/com/algaworks/api/algafood/api/exceptionHandler/ProblemType.java
@@ -7,6 +7,7 @@ public enum ProblemType {
     ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
     ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+    PARAMETRO_INVALIDO("/parametro-invalido", "Parametro informado é inválido"),
     MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível");
 
     private String title;
