5d226bb48014386a6ccf52e9d064b9f49e848c41
Jota Santos <jailsonsantosdpge@outlook.com>
2025-10-14 16:09:15 -0400

    11.14. Adicionando e usando o ModelMapper

--- src/main/java/com/algaworks/api/algafood/api/assembler/RestauranteInputlDissassembler.java
+++ src/main/java/com/algaworks/api/algafood/api/assembler/RestauranteInputlDissassembler.java
@@ -1,31 +1,20 @@
 package com.algaworks.api.algafood.api.assembler;
 
-import com.algaworks.api.algafood.api.model.CozinhaModel;
-import com.algaworks.api.algafood.api.model.RestauranteModel;
-import com.algaworks.api.algafood.api.model.input.CozinhaIdInput;
 import com.algaworks.api.algafood.api.model.input.RestauranteInput;
-import com.algaworks.api.algafood.domain.model.Cozinha;
 import com.algaworks.api.algafood.domain.model.Restaurante;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 @Component
 public class RestauranteInputlDissassembler {
+
+    private final ModelMapper modelMapper;
+
+    public RestauranteInputlDissassembler(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
     public Restaurante toModel(RestauranteInput restauranteInput) {
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteInput.getNome());
-        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-        restaurante.setNome(restauranteInput.getNome());
-
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteInput.getCozinha().getId());
-        restaurante.setCozinha(cozinha);
-
-        return restaurante;
+        return modelMapper.map(restauranteInput, Restaurante.class);
     }
 }
--- src/main/java/com/algaworks/api/algafood/api/assembler/RestauranteModelAssembler.java
+++ src/main/java/com/algaworks/api/algafood/api/assembler/RestauranteModelAssembler.java
@@ -3,24 +3,22 @@ package com.algaworks.api.algafood.api.assembler;
 import com.algaworks.api.algafood.api.model.CozinhaModel;
 import com.algaworks.api.algafood.api.model.RestauranteModel;
 import com.algaworks.api.algafood.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class RestauranteModelAssembler {
+
+    private final ModelMapper modelMapper;
+
+    public RestauranteModelAssembler(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
     public RestauranteModel toModel(Restaurante restaurante) {
-        CozinhaModel cozinhaModel = new CozinhaModel();
-        cozinhaModel.setId(restaurante.getId());
-        cozinhaModel.setNome(restaurante.getCozinha().getNome());
-
-        RestauranteModel restauranteModel = new RestauranteModel();
-        restauranteModel.setId(restaurante.getId());
-        restauranteModel.setNome(restaurante.getNome());
-        restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
-        restauranteModel.setCozinha(cozinhaModel);
-
-        return restauranteModel;
+        return modelMapper.map(restaurante, RestauranteModel.class);
     }
 
     public List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes) {
